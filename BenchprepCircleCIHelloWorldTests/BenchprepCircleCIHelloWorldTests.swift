ffbc0d029b449dcfc26705ab48fc7e841a30ca9b
rdalin82 <rdalin@benchprep.com>
2021-06-21 16:24:11 -0500

    Add fastlane and circle ci config

--- BenchprepCircleCIHelloWorldTests/BenchprepCircleCIHelloWorldTests.swift
+++ BenchprepCircleCIHelloWorldTests/BenchprepCircleCIHelloWorldTests.swift
@@ -19,6 +19,7 @@ class BenchprepCircleCIHelloWorldTests: XCTestCase {
     }
 
     func testExample() throws {
+        XCTAssertEqual(2, 1+1)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
